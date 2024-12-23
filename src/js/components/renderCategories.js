aa518a29287ae2d25e027cbda33962a51046e681
BruMaster7 <brunorocha1ero4@gmail.com>
2024-12-23 02:29:11 -0300

    Fix hero category

--- src/js/components/renderCategories.js
+++ src/js/components/renderCategories.js
@@ -9,9 +9,8 @@ export const renderCategories = (categories) => {
       .map(
         (category) => `
           <button type="button" 
-                  class="focus:outline-none text-justa-white font-bold text-sm bg-justa-red hover:bg-green-800 
-                         focus:ring-4 rounded-full focus:ring-green-300 px-5 py-2.5 me-2 mb-2 dark:bg-justa-red 
-                         dark:hover:bg-red-700 dark:focus:ring-red-800 my-3"
+                  class="focus:outline-none text-justa-white font-bold text-sm bg-justa-green hover:bg-green-800 
+                         focus:ring-4 rounded-full focus:ring-green-300 px-5 py-2.5 me-2 mb-2 my-3"
                   data-category="${category}">
             ${category}
           </button>
@@ -21,9 +20,8 @@ export const renderCategories = (categories) => {
   } else {
     return `
       <button type="button" 
-              class="focus:outline-none text-justa-white font-bold text-sm bg-justa-red hover:bg-green-800 
-                     focus:ring-4 rounded-full focus:ring-green-300 px-5 py-2.5 me-2 mb-2 dark:bg-justa-red 
-                     dark:hover:bg-red-700 dark:focus:ring-red-800 my-3"
+              class="focus:outline-none text-justa-white font-bold text-sm bg-justa-green hover:bg-green-800 
+                         focus:ring-4 rounded-full focus:ring-green-300 px-5 py-2.5 me-2 mb-2 my-3"
               data-category="${categories}">
         ${categories}
       </button>
--- src/js/components/renderHero.js
+++ src/js/components/renderHero.js
@@ -1,5 +1,6 @@
 import { getLogo } from "../utils/getLogo.js";
 import {getLink} from "../utils/getLink.js"
+import { renderCategories } from "./renderCategories.js";
 
 export const renderHero = (newsArray) => {
   const randomNews = newsArray[Math.floor(Math.random() * newsArray.length)];
@@ -10,11 +11,7 @@ export const renderHero = (newsArray) => {
       <div class="absolute inset-0 bg-black opacity-50"></div>
       <div class="max-w-5xl min-h-full mx-auto relative z-10 py-10 px-5 flex flex-col justify-center mb-5">
         <div class="category mb-5 mt-5">
-          <a href="#">
-            <button class="focus:outline-none text-justa-white font-bold text-xl bg-justa-green hover:bg-green-800 px-5 py-2.5 rounded-full">
-              ${randomNews.category || "Categoría desconocida"}
-            </button>
-          </a>
+            <div id="categories-container">${renderCategories(randomNews.category)}</div>
         </div>
         <div class="hero-new max-w-screen-md mt-5">
           <h3 class="text-4xl font-bold text-white">${randomNews.title}</h3>
